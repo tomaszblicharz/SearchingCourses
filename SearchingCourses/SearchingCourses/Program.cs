70671f2a816da156a76f94c53b1f9b108c6f6f44
Tomasz <t.blicharz97@gmail.com>
2019-05-22 16:46:30 +0200

    koniec w czw

--- SearchingCourses/SearchingCourses/Program.cs
+++ SearchingCourses/SearchingCourses/Program.cs
@@ -10,9 +10,20 @@ namespace SearchingCourses
     {
         public static void Main()
         {
-            var songLyrics = new SongLyrics("shakira", "Nada");
+            //var songLyrics = new SongLyrics("shakira", "Nada");
+            var profanityFinder = new ProfanityFinder ("no, kurwa!");
             Console.WriteLine("Done.");
             Console.ReadLine();
+        }
+    }
+
+    internal class ProfanityFinder
+    {
+       
+
+        public ProfanityFinder(string text)
+        {
+            var dictFile = File.ReadAllText(path:"profanities.txt")
         }
     }
 }

a31ebe4bcc282ae949457a2a7131a4508a203f8a
Tomasz <t.blicharz97@gmail.com>
2019-05-22 16:10:33 +0200

    Shakira

--- SearchingCourses/SearchingCourses/Program.cs
+++ SearchingCourses/SearchingCourses/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,25 @@ namespace SearchingCourses
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            var songLyrics = new SongLyrics("shakira", "Nada");
+            Console.WriteLine("Done.");
+            Console.ReadLine();
         }
     }
+
+    class SongLyrics
+    {
+        public SongLyrics(string artist, string title)
+        {
+            var browser = new WebClient();
+            var url = "https://api.lyrics.ovh/v1/" + artist + "/" + title;
+            var json = browser.DownloadString(url);
+            Console.WriteLine(json);
+
+        }
+    }
+
 }
+

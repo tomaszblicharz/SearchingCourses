f24bc4e54d2cd2be3df8ca29f60bc7b4e5315e66
Tomasz <t.blicharz97@gmail.com>
2019-05-22 16:26:04 +0200

    uporzadkowane

--- SearchingCourses/SearchingCourses/Program.cs
+++ SearchingCourses/SearchingCourses/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,18 +15,5 @@ namespace SearchingCourses
             Console.ReadLine();
         }
     }
-
-    class SongLyrics
-    {
-        public SongLyrics(string artist, string title)
-        {
-            var browser = new WebClient();
-            var url = "https://api.lyrics.ovh/v1/" + artist + "/" + title;
-            var json = browser.DownloadString(url);
-            Console.WriteLine(json);
-
-        }
-    }
-
 }
 
